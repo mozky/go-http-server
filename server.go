25c38d775992207190d68bdc0dac10b74ebafb3b
mozky <javiersuarezl@outlook.com>
2019-04-10 14:37:31 -0500

    Added path variable URL

--- server.go
+++ server.go
@@ -10,11 +10,20 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hi there!\n"))
 }
 
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	username := vars["username"]
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hi " + username))
+	log.Printf("Request for user %v\n", username)
+}
+
 func main() {
 	r := mux.NewRouter()
-	log.Print("Starting new server on :8000")
+	log.Print("Starting new server on :8000 😀")
 
 	r.HandleFunc("/", Handler)
+	r.HandleFunc("/user/{username}", UserHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
